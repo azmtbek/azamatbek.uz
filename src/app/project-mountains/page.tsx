5f902a13ccc45a5b3101d17d60f79777e647ec09
Azamatbek <39667415+azmtbek@users.noreply.github.com>
2023-09-25 23:19:31 +0500

    update moon

--- src/app/project-mountains/page.tsx
+++ src/app/project-mountains/page.tsx
@@ -24,7 +24,14 @@ const Projects = () => {
         isTame && "opacity-100 translate-x-0",
       )}
     >
-      {theme == "dark" && <div className="moon top-20 right-10 -z-10"></div>}
+      {theme == "dark" && (
+        <>
+          <div className="moon top-20 right-10 -z-20"></div>
+          <div className="moon2 top-24 right-16 -z-10"></div>
+          <div className="moon3 top-32 right-16 -z-10"></div>
+          <div className="moon4 top-24 right-20 -z-10"></div>
+        </>
+      )}
       <h1>Here shall be Projects</h1>
     </div>
   );
