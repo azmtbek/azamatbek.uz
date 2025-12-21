8faa578cead3f673f4d3e54497efe0eca2b7a436
aza <azamatbek.dev@gmail.com>
2025-12-22 03:47:25 +0500

    clean up

--- src/app/(pages)/arcade/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function page() {
-  return (
-    <div>Arcade</div>
-  )
-}
-
-export default page
--- src/app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function NotFound() {
-  return (
-    <div className='flex h-screen w-screen justify-center items-center'>404</div>
-  )
-}
-
-export default NotFound
